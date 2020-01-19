95bf02d938bb8463740ec68fa1a599d6c4549785
alberto perez <albertoperez@MBP-de-alberto.local.tld>
2020-01-19 14:40:01 -0600

    clase 21 se agrega recursos externos del index.html

--- clima/src/components/WeatherExtraInfo.js
+++ clima/src/components/WeatherExtraInfo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import WeatherIcons from 'react-weathericons';
 const WeatherExtraInfo = ({humididty,wind}) =>{
 
     // template script example
     return (<div>
     <span>{`${humididty} %`}</span>
+    <WeatherIcons name="cloud" size="2x" />
     <br/>
     <span>{`${wind} %`}</span>
     </div>);
