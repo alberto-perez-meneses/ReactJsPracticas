133c9bd1f6bc7904c96492997541749f0a42d9ec
alberto perez <albertoperez@MBP-de-alberto.local.tld>
2020-01-19 13:50:17 -0600

    alcance clase 14  creacion de componentes

---
+++ clima/src/components/Location.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Location = () => (
+    <h1>México</h1>
+);
+
+export default Location;
---
+++ clima/src/components/WeatherExtraInfo.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const WeatherExtraInfo = () => (
+ <div>Weather Extra Info</div>
+);
+
+export default WeatherExtraInfo;
